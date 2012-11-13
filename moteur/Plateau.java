fbdc235d94cfe06dbc6e5628f189042b4e6c5d60
chappellet.kevin@gmail.com <chappellet.kevin@gmail.com@6f96fe11-dc5f-5d4e-c873-46718da31d97>
2012-11-13 15:37:46 +0000

    Ajout des methodes dans Pion

--- moteur/Plateau.java
+++ moteur/Plateau.java
@@ -9,55 +9,53 @@ import javax.swing.JComponent;
  * @author Kévin
  */
 public class Plateau extends JComponent {
-	
-	/**
-	 * Nombre de colonne dans le plateau
-	 */
-    private static int colonne = 20;
-	
-	/**
-	 * Nombre de ligne dans le plateau
-	 */
-	private static int ligne = 12;
-	
+
+    /**
+     * Nombre de colonne dans le plateau
+     */
+    private static int colonne = 24;
+    /**
+     * Nombre de ligne dans le plateau
+     */
+    private static int ligne = 18;
     /**
      * Tableau de Cases symbolisant la plateau.
      */
     private Case[][] plateau;
-    
+
     /**
      * Constructeur de la classe Plateau.
      */
     public Plateau() {
         super();
-        this.setPreferredSize(new Dimension((colonne)*Case.TAILLE,(ligne)*Case.TAILLE));
+        this.setPreferredSize(new Dimension((colonne) * Case.TAILLE, (ligne) * Case.TAILLE));
         plateau = new Case[ligne][colonne];
         /* Initialisation des cases blanches */
-        for(int i=0;i<ligne;i++) {
-            for(int j=0;j<colonne;j++) {
-				if(i == ligne-1 || j == colonne-1){
-					plateau[i][j] = new Case(i,j,this, true);
-				}
-				else
-					plateau[i][j] = new Case(i,j,this, false);
-                get(i,j).setBounds(j*Case.TAILLE,i*Case.TAILLE,Case.TAILLE,Case.TAILLE);
-                add(get(i,j));
+        for (int i = 0; i < ligne; i++) {
+            for (int j = 0; j < colonne; j++) {
+                if (i == ligne - 1 || j == 0) {
+                    plateau[i][j] = new Case(i, j, this, true);
+                } else {
+                    plateau[i][j] = new Case(i, j, this, false);
+                }
+                get(i, j).setBounds(j * Case.TAILLE, i * Case.TAILLE, Case.TAILLE, Case.TAILLE);
+                add(get(i, j));
             }
         }
     }
-	
+
     /**
      * Retourne la case situé à la même position, c'est-à-dire même ligne et même colonne. 
      * @param c Case qui est retournée
      * @return Retourne la case c.
      */
     public Case get(Case c) {
-        if(c!=null) {
+        if (c != null) {
             return plateau[c.getLigne()][c.getColonne()];
         }
         return null;
     }
-    
+
     /**
      * Retourne la case se situant à la ligne l,et à la colonne c.
      * @param l ligne de la case.
@@ -66,47 +64,50 @@ public class Plateau extends JComponent {
      */
     public Case get(int l, int c) {
         //Arguments sont valides
-        if(l>=0 && l<ligne && c>=0 && c<colonne)
+        if (l >= 0 && l < ligne && c >= 0 && c < colonne) {
             return plateau[l][c];
-        
+        }
+
         return null;
-        
+
     }
-    
+
     /**
      * Méthode retournant la hauteur du plateau
      * @return hauteur du plateau
      */
-	@Override
+    @Override
     public int getHeight() {
-        return ligne*Case.TAILLE;
+        return ligne * Case.TAILLE;
     }
-    
+
     /**
      * Retourne la largeur du tableau
      * @return largeur du tableau
      */
-	@Override
+    @Override
     public int getWidth() {
-        return colonne*Case.TAILLE;
+        return colonne * Case.TAILLE;
     }
-	
-    
+
     /**
      * Supprime les pieces se situant sur la plateau.
      *
      */
     public void vider() {
-        for(int i=0;i<ligne;i++)
-            for(int j=0;j<colonne;j++)
-                get(i,j).remove();
+        for (int i = 0; i < ligne; i++) {
+            for (int j = 0; j < colonne; j++) {
+                get(i, j).remove();
+            }
+        }
     }
-	
-	
-	@Override
-    public void paintComponent(Graphics g){
-		for(int i=0;i<ligne;i++)
-            for(int j=0;j<colonne;j++)
-				plateau[i][j].repaint();
-	}
+
+    @Override
+    public void paintComponent(Graphics g) {
+        for (int i = 0; i < ligne; i++) {
+            for (int j = 0; j < colonne; j++) {
+                plateau[i][j].repaint();
+            }
+        }
+    }
 }
