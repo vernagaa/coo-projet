45eff890151040f8b8342e5b1e2fd4368b9cb0d0
chappellet.kevin@gmail.com <chappellet.kevin@gmail.com@6f96fe11-dc5f-5d4e-c873-46718da31d97>
2012-11-22 11:53:48 +0000

    Calcul de la zone de deplacement et affichage de l'aire de deplacement

--- trunk/ihm/AireDeJeu.java
+++ trunk/ihm/AireDeJeu.java
@@ -15,51 +15,69 @@ import moteur.Textures;
  */
 public class AireDeJeu extends JComponent {
 
-	private Plateau plateau;
+    private Plateau plateau;
 
-	public AireDeJeu(Plateau plateau) {
-		setPreferredSize(new Dimension(plateau.getNbColonne() * Case.TAILLE + 1, plateau.getNbLigne() * Case.TAILLE + 1));
-		this.plateau = plateau;
-	}
+    public AireDeJeu(Plateau plateau) {
+        setPreferredSize(new Dimension(plateau.getNbColonne() * Case.TAILLE + 1, plateau.getNbLigne() * Case.TAILLE + 1));
+        this.plateau = plateau;
+    }
 
-	public AireDeJeu() {
-		this(new Plateau());
-	}
+    public AireDeJeu() {
+        this(new Plateau());
+    }
 
-	public Plateau getPlateau() {
-		return plateau;
-	}
+    public Plateau getPlateau() {
+        return plateau;
+    }
 
-	public void setPlateau(Plateau plateau) {
-		this.plateau = plateau;
-	}
+    public void setPlateau(Plateau plateau) {
+        this.plateau = plateau;
+    }
 
-	@Override
-	public void paintComponent(Graphics g) {
-		Graphics2D gd = (Graphics2D) g;
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D gd = (Graphics2D) g;
 
-		for (Case[] c : plateau.get()) {
-			for (Case c1 : c) {
-				gd.drawImage(Textures.getTerrain(c1.getTypeTerrain()), c1.getColonne() * Case.TAILLE, c1.getLigne() * Case.TAILLE, null);
-				if (c1.getBordure() != null) {
-					gd.drawImage(Textures.getBordure(c1.getBordure().getTypeBordure()), c1.getColonne() * Case.TAILLE, c1.getLigne() * Case.TAILLE, null);
-				}
-				if (c1.getObstacle() != null) {
-					gd.drawImage(Textures.getObstacle(c1.getObstacle().getTypeObstacle()), c1.getColonne() * Case.TAILLE, c1.getLigne() * Case.TAILLE, null);
-				}
-				if (c1.getPion() != null) {
-					gd.drawImage(c1.getPion().getImage(), c1.getColonne() * Case.TAILLE, c1.getLigne() * Case.TAILLE, null);
-				}
-			}
-		}
-		gd.setColor(new Color(80, 80, 80, 40));
+        for (Case[] c : plateau.get()) {
+            for (Case c1 : c) {
+                gd.drawImage(Textures.getTerrain(c1.getTypeTerrain()), c1.getColonne() * Case.TAILLE, c1.getLigne() * Case.TAILLE, null);
+                if (c1.getBordure() != null) {
+                    gd.drawImage(Textures.getBordure(c1.getBordure().getTypeBordure()), c1.getColonne() * Case.TAILLE, c1.getLigne() * Case.TAILLE, null);
+                }
+                if (c1.getObstacle() != null) {
+                    gd.drawImage(Textures.getObstacle(c1.getObstacle().getTypeObstacle()), c1.getColonne() * Case.TAILLE, c1.getLigne() * Case.TAILLE, null);
+                }
+                if (c1.getPion() != null) {
+                    gd.drawImage(c1.getPion().getImage(), c1.getColonne() * Case.TAILLE, c1.getLigne() * Case.TAILLE, null);
+                }
+            }
+        }
+        for (Case[] c : plateau.get()) {
+            for (Case c1 : c) {
+                if (c1.getPion() != null) {
+                    if (!c1.getPion().listeDeplacement.isEmpty()) {
+                        int i = 0;
+                        for (Case c2 : c1.getPion().listeDeplacement) {
+                            gd.setColor(new Color(25, 150, 255, 100-i));
+                            gd.fillRect(c2.getLigne() * Case.TAILLE, c2.getColonne() * Case.TAILLE, Case.TAILLE, Case.TAILLE);
+                            i += 1;
+                        }
+                    }
+                }
 
-		for (int i = 0; i <= plateau.getNbLigne() * Case.TAILLE; i += Case.TAILLE) {
-			gd.drawLine(0, i, plateau.getNbColonne() * Case.TAILLE, i);
-		}
-		for (int i = 0; i <= plateau.getNbColonne() * Case.TAILLE; i += Case.TAILLE) {
-			gd.drawLine(i, 0, i, plateau.getNbLigne() * Case.TAILLE);
-		}
+            }
+        }
 
-	}
+
+        gd.setColor(new Color(80, 80, 80, 40));
+
+        for (int i = 0; i < getHeight(); i += Case.TAILLE) {
+            gd.drawLine(0, i, getWidth(), i);
+        }
+        for (int j = 0; j < getWidth(); j += Case.TAILLE) {
+            gd.drawLine(j, 0, j, getHeight());
+        }
+
+
+    }
 }
--- trunk/moteur/Plateau.java
+++ trunk/moteur/Plateau.java
@@ -6,6 +6,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import moteur.familles.oiseau.AssassinOiseau;
 import moteur.familles.oiseau.GuerrierOiseau;
+import moteur.familles.oiseau.TankOiseau;
 
 /**
  *
@@ -13,130 +14,141 @@ import moteur.familles.oiseau.GuerrierOiseau;
  */
 public class Plateau implements Serializable {
 
-	/**
-	 * Nombre de colonne dans le plateau
-	 */
-	private int colonne = 35;
-	/**
-	 * Nombre de ligne dans le plateau
-	 */
-	private int ligne = 20;
-	/**
-	 * Tableau de Cases symbolisant la plateau.
-	 */
-	private Case[][] plateau;
+    protected static final long serialVersionUID = 1L;
+    /**
+     * Nombre de colonne dans le plateau
+     */
+    private int colonne = 35;
+    /**
+     * Nombre de ligne dans le plateau
+     */
+    private int ligne = 20;
+    /**
+     * Tableau de Cases symbolisant la plateau.
+     */
+    private Case[][] plateau;
 
-	/**
-	 * Constructeur de la classe Plateau.
-	 */
-	public Plateau() {
-		super();
-		plateau = new Case[ligne][colonne];
-		/*
-		 * Initialisation des cases blanches
-		 */
-		for (int i = 0; i < ligne; i++) {
-			for (int j = 0; j < colonne; j++) {
-				plateau[i][j] = new Case(i, j, this);
-			}
-		}
-	}
+    /**
+     * Constructeur de la classe Plateau.
+     */
+    public Plateau() {
+        super();
+        plateau = new Case[ligne][colonne];
+        /*
+         * Initialisation des cases blanches
+         */
+        for (int i = 0; i < ligne; i++) {
+            for (int j = 0; j < colonne; j++) {
+                plateau[i][j] = new Case(i, j, this);
+            }
+        }
+    }
 
-	/**
-	 * Constructeur avec une carte prédéfinie
-	 *
-	 * @param f
-	 */
-	public Plateau(String str) {
-		plateau = new Case[ligne][colonne];
-		File fichier = new File(str);
-		FileInputStream fis = null;
-		try {
-			fis = new FileInputStream(fichier);
-		} catch (FileNotFoundException ex) {
-			Logger.getLogger(FenetreEditeur.class.getName()).log(Level.SEVERE, null, ex);
-		}
+    /**
+     * Constructeur avec une carte prédéfinie
+     *
+     * @param f
+     */
+    public Plateau(String str) {
+        plateau = new Case[ligne][colonne];
+        File fichier = new File(str);
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(fichier);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FenetreEditeur.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-		if (fis != null) {
-			ObjectInputStream ois = null;
-			try {
-				ois = new ObjectInputStream(fis);
-			} catch (IOException ex) {
-				Logger.getLogger(FenetreEditeur.class.getName()).log(Level.SEVERE, null, ex);
-			} finally {
-				try {
-					Plateau p = (Plateau) ois.readObject();
-					for (int i = 0; i < plateau.length; i++) {
-						for (int j = 0; j < plateau[i].length; j++) {
-							plateau[i][j] = new Case(i, j, this);
-							plateau[i][j].setBordure(p.get(i, j).getBordure());
-							plateau[i][j].setObstacle(p.get(i, j).getObstacle());
-							plateau[i][j].setTypeTerrain(p.get(i, j).getTypeTerrain());
-						}
-					}
-					ois.close();
-					fis.close();
-				} catch (IOException ex) {
-					Logger.getLogger(FenetreEditeur.class.getName()).log(Level.SEVERE, null, ex);
-				} catch (ClassNotFoundException ex) {
-					Logger.getLogger(Plateau.class.getName()).log(Level.SEVERE, null, ex);
-				}
-			}
-		}
-	}
+        if (fis != null) {
+            ObjectInputStream ois = null;
+            try {
+                ois = new ObjectInputStream(fis);
+            } catch (IOException ex) {
+                Logger.getLogger(FenetreEditeur.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    Plateau p = (Plateau) ois.readObject();
+                    for (int i = 0; i < plateau.length; i++) {
+                        for (int j = 0; j < plateau[i].length; j++) {
+                            plateau[i][j] = new Case(i, j, this);
+                            plateau[i][j].setBordure(p.get(i, j).getBordure());
+                            plateau[i][j].setObstacle(p.get(i, j).getObstacle());
+                            plateau[i][j].setTypeTerrain(p.get(i, j).getTypeTerrain());
+                        }
+                    }
+                    ois.close();
+                    fis.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(FenetreEditeur.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(Plateau.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        
+        AssassinOiseau o = new AssassinOiseau(plateau[9][5]);
+       
+        System.out.println(plateau[5][5].getPion());
+        System.out.println(o.deplacementPossible(plateau[7][7]));
+        
+    }
 
-	/**
-	 * Retourne la case situé à la même position, c'est-à-dire même ligne et
-	 * même colonne.
-	 *
-	 * @param c Case qui est retournée
-	 * @return Retourne la case c.
-	 */
-	public Case get(Case c) {
-		if (c != null) {
-			return plateau[c.getLigne()][c.getColonne()];
-		}
-		return null;
-	}
+    /**
+     * Retourne la case situé à la même position, c'est-à-dire même ligne et
+     * même colonne.
+     *
+     * @param c Case qui est retournée
+     * @return Retourne la case c.
+     */
+    public Case get(Case c) {
+        if (c != null) {
+            return plateau[c.getLigne()][c.getColonne()];
+        }
+        return null;
+    }
 
-	/**
-	 * Retourne la case se situant à la ligne l,et à la colonne c.
-	 *
-	 * @param l ligne de la case.
-	 * @param c colonne de la case.
-	 * @return Case situé en position l,c
-	 */
-	public Case get(int l, int c) {
-		//Arguments sont valides
-		if (l >= 0 && l < ligne && c >= 0 && c < colonne) {
-			return plateau[l][c];
-		}
+    /**
+     * Retourne la case se situant à la ligne l,et à la colonne c.
+     *
+     * @param l ligne de la case.
+     * @param c colonne de la case.
+     * @return Case situé en position l,c
+     */
+    public Case get(int l, int c) {
+        //Arguments sont valides
+        if (l >= 0 && l < ligne && c >= 0 && c < colonne) {
+            return plateau[l][c];
+        }
 
-		return null;
+        return null;
 
-	}
+    }
 
-	public Case[][] get() {
-		return plateau;
-	}
+    public Case[][] get() {
+        return plateau;
+    }
 
-	/**
-	 * Supprime les pieces se situant sur la plateau.
-	 *
-	 */
-	public void vider() {
-		for (int i = 0; i < ligne; i++) {
-			for (int j = 0; j < colonne; j++) {
-				get(i, j).remove();
-			}
-		}
-	}
+    /**
+     * Supprime les pieces se situant sur la plateau.
+     *
+     */
+    public void vider() {
+        for (int i = 0; i < ligne; i++) {
+            for (int j = 0; j < colonne; j++) {
+                get(i, j).remove();
+            }
+        }
+    }
 
-	public int getNbColonne() {
-		return colonne;
-	}
+    public int getNbColonne() {
+        return colonne;
+    }
 
-	public int getNbLigne() {
-		return ligne;
-	}
+    public int getNbLigne() {
+        return ligne;
+    }
+
+    public boolean deplacementPossible(Case c1, Case c2) {
+        return c1.getPion().deplacementPossible(c2);
+    }
 }
