03b523e2e059e3fe340520c4ed920907c6001b2c
chappellet.kevin@gmail.com <chappellet.kevin@gmail.com@6f96fe11-dc5f-5d4e-c873-46718da31d97>
2012-11-26 15:46:13 +0000

    Blablablabla

--- moteur/Moteur.java
+++ moteur/Moteur.java
@@ -1,5 +1,6 @@
 package moteur;
 
+import ihm.AireDeJeu;
 import ihm.FenetrePrincipale;
 import java.io.*;
 import javax.swing.SwingUtilities;
@@ -10,28 +11,50 @@ import javax.swing.SwingUtilities;
  */
 public class Moteur implements Runnable, Serializable {
 
-	FenetrePrincipale fp;
-	Plateau plateau;
+    private FenetrePrincipale fp;
+    private Plateau plateau;
+    private AireDeJeu aireDeJeu;
+    
+    private Case caseCourante;
+    private Case caseDebutMouvement;
+    boolean mouvement;
 
-	public Plateau getPlateau() {
-		return plateau;
-	}
+    public Plateau getPlateau() {
+        return plateau;
+    }
 
-	public void setPlateau(Plateau plateau) {
-		this.plateau = plateau;
-	}
+    public void setPlateau(Plateau plateau) {
+        this.plateau = plateau;
+    }
 
-	public Moteur() {
-		plateau = new Plateau("map/map1.map");
-	}
+    public Moteur() {
+        plateau = new Plateau("map/map2.map");
+    }
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Moteur());
-	}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Moteur());
+    }
 
-	@Override
-	public void run() {
-		fp = new FenetrePrincipale(this);
-	}
+    @Override
+    public void run() {
+        fp = new FenetrePrincipale(this);
+        aireDeJeu = fp.getAireDeJeu();
+    }
 
+    public void caseCliqueBoutonGauche(Case c1) {
+        if (caseCourante != null) {
+            caseCourante.setSelect(false);
+        }
+        
+        caseCourante = c1;
+        
+//        Selectionne un Pion
+        if(c1.getPion() != null){
+            mouvement = true;
+            caseCourante.setSelect(true);
+            caseDebutMouvement = caseCourante;
+        }
+        
+        aireDeJeu.repaint();
+    }
 }
