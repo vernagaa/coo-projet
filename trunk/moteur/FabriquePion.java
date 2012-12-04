298bd84f0c601ee774ddf11d9833fd43925e283a
anthony.vernagallo@gmail.com <anthony.vernagallo@gmail.com@6f96fe11-dc5f-5d4e-c873-46718da31d97>
2012-12-04 09:00:58 +0000

    Fabrique complétée

--- trunk/moteur/FabriquePion.java
+++ trunk/moteur/FabriquePion.java
@@ -1,5 +1,7 @@
 package moteur;
 
+import moteur.familles.felin.*;
+import moteur.familles.oiseau.*;
 import moteur.familles.reptile.*;
 
 /**
@@ -8,36 +10,77 @@ import moteur.familles.reptile.*;
  */
 public class FabriquePion {
 
-	public final static int FELIN = 0;
-	public final static int OISEAU = 1;
-	public final static int REPTILE = 2;
+	public static final int FELIN = 0;
+	public static final int OISEAU = 1;
+	public static final int REPTILE = 2;
 	
-	public final static int ARCHER = 0;
-	public final static int ASSASSIN = 1;
-	public final static int GUERRIER = 2;
-	public final static int TACTICIEN = 3;
-	public final static int TANK = 4;
+	public static final int ARCHER = 0;
+	public static final int ASSASSIN = 1;
+	public static final int GUERRIER = 2;
+	public static final int TACTICIEN = 3;
+	public static final int TANK = 4;
 	
 	public Pion getPion(int famille, int classe, Case c) {
 		switch (famille) {
 			case FELIN:
-				break;
+				return pionFelin(classe, c);
 			case OISEAU:
-				break;
+				return pionOiseau(classe, c);
 			case REPTILE:
 				return pionReptile(classe, c);
 			default:
+				return null;
 		}
-		return null;
+	}
+
+	private Pion pionFelin(int classe, Case c) {
+		switch (classe) {
+			case ARCHER:
+				return new ArcherFelin(c);
+			case ASSASSIN:
+				return new AssassinFelin(c);
+			case GUERRIER:
+				return new GuerrierFelin(c);
+			case TACTICIEN:
+				return new TacticienFelin(c);
+			case TANK:
+				return new TankFelin(c);
+			default:
+				return null;
+		}
+	}
+
+	private Pion pionOiseau(int classe, Case c) {
+		switch (classe) {
+			case ARCHER:
+				return new ArcherOiseau(c);
+			case ASSASSIN:
+				return new AssassinOiseau(c);
+			case GUERRIER:
+				return new GuerrierOiseau(c);
+			case TACTICIEN:
+				return new TacticienOiseau(c);
+			case TANK:
+				return new TankOiseau(c);
+			default:
+				return null;
+		}
 	}
 
 	private Pion pionReptile(int classe, Case c) {
 		switch (classe) {
 			case ARCHER:
 				return new ArcherReptile(c);
+			case ASSASSIN:
+				return new AssassinReptile(c);
 			case GUERRIER:
 				return new GuerrierReptile(c);
+			case TACTICIEN:
+				return new TacticienReptile(c);
+			case TANK:
+				return new TankReptile(c);
+			default:
+				return null;
 		}
-		return null;
 	}
 }
