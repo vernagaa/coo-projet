ebacd5f4315bfb4958abb1a818f232d8dd91f315
remi.disavino <remi.disavino@6f96fe11-dc5f-5d4e-c873-46718da31d97>
2012-12-10 10:07:44 +0000

    javadoc editeur

--- trunk/editeur/Editeur.java
+++ trunk/editeur/Editeur.java
@@ -12,22 +12,40 @@ public class Editeur implements Runnable {
 	FenetreEditeur fe;
 	Plateau plateau;
 
+	/**
+	 * Accesseur du plateau d'édition
+	 * @return plateau
+	 */
 	public Plateau getPlateau() {
 		return plateau;
 	}
 
+	/**
+	 * Affecteur du plateau d'édition
+	 * @param plateau
+	 */
 	public void setPlateau(Plateau plateau) {
 		this.plateau = plateau;
 	}
 
+	/**
+	 * Constructeur de l'éditeur
+	 */
 	public Editeur() {
 		plateau = new Plateau();
 	}
 
+	/**
+	 * Main de l'éditeur
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Editeur());
 	}
 
+	/**
+	 * Création de la fenêtre d'édition
+	 */
 	@Override
 	public void run() {
 		fe = new FenetreEditeur(this);
