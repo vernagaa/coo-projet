75a6cf52519741fed770eb6448821db77c20c014
remi.disavino <remi.disavino@6f96fe11-dc5f-5d4e-c873-46718da31d97>
2012-12-11 13:23:12 +0000

    javadoc bordure et case isOccupee() et contientPion() fusionnées

--- moteur/Bordure.java
+++ moteur/Bordure.java
@@ -8,17 +8,32 @@ import java.io.Serializable;
  */
 public final class Bordure implements Serializable {
 
+	/**
+	 * ID de serrializable
+	 */
 	protected static final long serialVersionUID = 1L;
 	private int typeBordure;
 
+	/**
+	 * Constructeur de bordure
+	 * @param typeBordure
+	 */
 	public Bordure(int typeBordure) {
 		this.typeBordure = typeBordure;
 	}
 
+	/**
+	 * Affecteur de typeBordure
+	 * @param typeBordure
+	 */
 	public void setTypeBordure(int typeBordure) {
 		this.typeBordure = typeBordure;
 	}
 
+	/**
+	 * Accesseur de typeBordure
+	 * @return
+	 */
 	public int getTypeBordure() {
 		return typeBordure;
 	}
