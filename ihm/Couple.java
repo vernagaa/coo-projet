849715df013c996e2e15aa2c0b0bd07e23923898
anthony.vernagallo@gmail.com <anthony.vernagallo@gmail.com@6f96fe11-dc5f-5d4e-c873-46718da31d97>
2012-12-11 17:02:28 +0000

    + Centrage de la prévisualisation + Javadoc

--- ihm/Couple.java
+++ ihm/Couple.java
@@ -1,7 +1,7 @@
 package ihm;
 
 /**
- *
+ * Couple d'objets
  * @author vernagaa
  */
 public class Couple<K,V> {
@@ -9,23 +9,44 @@ public class Couple<K,V> {
 	private K key;
 	private V value;
 
+	/**
+	 * Constructeur du couple
+	 * @param val1 Objet clé
+	 * @param val2 Objet valeur
+	 */
 	public Couple(K val1, V val2) {
 		this.key = val1;
 		this.value = val2;
 	}
 
+	/**
+	 * Getter de l'objet clé
+	 * @return 
+	 */
 	public K getKey() {
 		return key;
 	}
 
+	/**
+	 * Setter de l'objet clé
+	 * @param key 
+	 */
 	public void setKey(K key) {
 		this.key = key;
 	}
 
+	/**
+	 * Getter de l'objet valeur
+	 * @return 
+	 */
 	public V getValue() {
 		return value;
 	}
 
+	/**
+	 * Setter de l'objet valeur
+	 * @param value 
+	 */
 	public void setValue(V value) {
 		this.value = value;
 	}
--- ihm/Previsualisation.java
+++ ihm/Previsualisation.java
@@ -9,25 +9,41 @@ import moteur.Plateau;
 import moteur.Textures;
 
 /**
- *
+ * Prévisualisation des cartes
  * @author vernagaa
  */
 public class Previsualisation extends JComponent {
 
 	private Plateau plateau;
 
+	/**
+	 * Constructeur
+	 * @param plateau plateau à prévisualiser
+	 */
 	public Previsualisation(Plateau plateau) {
 		this.plateau = plateau;
 	}
 
+	/**
+	 * Constructeur
+	 */
 	public Previsualisation() {
 		plateau = null;
 	}
 
+	/**
+	 * Getter du plateau prévisualisé
+	 * @return 
+	 */
 	public Plateau getPlateau() {
 		return plateau;
 	}
 
+	/**
+	 * Setter du plateau prévisualisé
+	 * La prévisualisation est ensuite actualisée
+	 * @param plateau 
+	 */
 	public void setPlateau(Plateau plateau) {
 		this.plateau = plateau;
 		repaint();
@@ -37,17 +53,22 @@ public class Previsualisation extends JComponent {
 	protected void paintComponent(Graphics g) {
 		Graphics2D gd = (Graphics2D) g;
 		
-		int caseTaille = Math.min((getWidth()-1)/Plateau.NB_COLONNE, (getHeight()-1)/Plateau.NB_LIGNE);
+		final int caseTaille = Math.min((getWidth()-1)/Plateau.NB_COLONNE, (getHeight()-1)/Plateau.NB_LIGNE);
+		final int hauteur = caseTaille * Plateau.NB_LIGNE;
+		final int largeur = caseTaille * Plateau.NB_COLONNE;
+		
+		final int decalH = (getHeight() - hauteur) / 2;
+		final int decalL = (getWidth() - largeur) / 2;
 		
 		if(plateau != null) {
 			for (Case[] c : plateau.get()) {
 				for (Case c1 : c) {
-					gd.drawImage(Textures.scale(Textures.getTerrain(c1.getTypeTerrain()), caseTaille, caseTaille), c1.getColonne() * caseTaille, c1.getLigne() * caseTaille, null);
+					gd.drawImage(Textures.scale(Textures.getTerrain(c1.getTypeTerrain()), caseTaille, caseTaille), c1.getColonne() * caseTaille + decalL, c1.getLigne() * caseTaille + decalH, null);
 					if (c1.getBordure() != null) {
-						gd.drawImage(Textures.scale(Textures.getBordure(c1.getBordure().getTypeBordure()), caseTaille, caseTaille), c1.getColonne() * caseTaille, c1.getLigne() * caseTaille, null);
+						gd.drawImage(Textures.scale(Textures.getBordure(c1.getBordure().getTypeBordure()), caseTaille, caseTaille), c1.getColonne() * caseTaille + decalL, c1.getLigne() * caseTaille + decalH, null);
 					}
 					if (c1.getObstacle() != null) {
-					gd.drawImage(Textures.scale(c1.getObstacle().getImage(), caseTaille, caseTaille), c1.getColonne() * caseTaille, c1.getLigne() * caseTaille, null);
+					gd.drawImage(Textures.scale(c1.getObstacle().getImage(), caseTaille, caseTaille), c1.getColonne() * caseTaille + decalL, c1.getLigne() * caseTaille + decalH, null);
 					}
 				}
 			}
@@ -55,12 +76,12 @@ public class Previsualisation extends JComponent {
 		
 		// Construit la grille du plateau	
 		gd.setColor(new Color(80, 80, 80, 40));
-//		gd.setColor(Color.BLACK);
+		
 		for (int i = 0; i <= Plateau.NB_LIGNE; i++) {
-			gd.drawLine(0, i * caseTaille, Plateau.NB_COLONNE * caseTaille, i * caseTaille);
+			gd.drawLine(decalL, i * caseTaille + decalH, Plateau.NB_COLONNE * caseTaille + decalL, i * caseTaille + decalH);
 		}
 		for (int j = 0; j <= Plateau.NB_COLONNE; j ++) {
-			gd.drawLine(j * caseTaille, 0, j * caseTaille, Plateau.NB_LIGNE * caseTaille);
+			gd.drawLine(j * caseTaille + decalL, decalH, j * caseTaille + decalL, Plateau.NB_LIGNE * caseTaille + decalH);
 		}
 	}
 	
